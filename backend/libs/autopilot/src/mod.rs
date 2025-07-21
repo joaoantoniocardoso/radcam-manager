cea4ced841efff7009a075c6718d15e017ed59d8
João Antônio Cardoso <joao.maker@gmail.com>
2025-07-21 16:15:15 -0300

    backend: libs: autopilot: Add Config

--- backend/libs/autopilot/src/mod.rs
+++ backend/libs/autopilot/src/mod.rs
@@ -5,9 +5,30 @@ pub mod routes;
 
 use anyhow::Result;
 use axum::Json;
+use serde::{Deserialize, Serialize};
 use tracing::*;
 
+use crate::parameters::{ActuatorsParameters, CLOSEST_POINTS, FURTHEST_POINTS};
+
 pub use routes::router;
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Config {
+    pub parameters: ActuatorsParameters,
+    pub closest_points: api::FocusZoomPoints,
+    pub furthest_points: api::FocusZoomPoints,
+    pub state: api::ActuatorsState,
+}
+
+impl Default for CameraActuators {
+    fn default() -> Self {
+        Self {
+            parameters: ActuatorsParameters::default(),
+            closest_points: api::FocusZoomPoints(CLOSEST_POINTS.to_vec()),
+            furthest_points: api::FocusZoomPoints(FURTHEST_POINTS.to_vec()),
+            state: api::ActuatorsState::default(),
+        }
+    }
+}
 
 #[instrument(level = "debug")]
 pub(crate) async fn control_inner(
